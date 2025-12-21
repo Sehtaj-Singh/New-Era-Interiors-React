651eb06fd5099cdc0c4e412f3e0c273dcc76bb0c
Sehtaj Singh <sehtajm49@gmail.com>
2025-12-21 13:16:34 +0530

    see

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production'
-    ? '/'                          // for custom domain
-    : '/new-era-interiors-react/'  // for local / repo usage
-}))
+  base: '/',   
+})
